bf825574789731ff2d5f6de58eb164ccbcdb6240
Tendai Mutunhire <tmutunhire@gmail.com>
2016-05-09 02:34:44 +0200

    delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,13 @@ class PostsController < ApplicationController
     redirect_to post_path @post
   end
 
+  def delete
+    @post = Post.find(params[:id])
+    @post.delete
+    flash[:notice] = "Post successfully deleted"
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
