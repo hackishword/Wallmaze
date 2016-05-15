dadf8dfeb2ae005f04567ad18003af79dc9dc0d2
Tendai Mutunhire <tmutunhire@gmail.com>
2016-05-15 21:56:57 +0200

    initial comment form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
   before_action :owned_post, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @posts = Post.all
