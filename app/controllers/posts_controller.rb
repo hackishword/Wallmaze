3916f3397bd7a05bce1299664b7220acd07f87b5
tomahawk <tmutunhire@gmail.com>
2016-08-15 22:55:33 +0200

    nifty pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC').page params[:page]
   end
 
   def new
