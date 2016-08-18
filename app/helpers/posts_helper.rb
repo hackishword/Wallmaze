fe52d3286b0b123f217abab27f25b42410454266
tomahawk <tmutunhire@gmail.com>
2016-08-18 15:33:29 +0200

    a marriage between Ruby on Rails and Javascript

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,8 @@
 module PostsHelper
-  def likers_of(post)
+  def display_likes(post)
     votes = post.votes_for.up.by_type(User)
-    user_names = []
-    unless votes.blank?
-      votes.voters.each do |voter|
-        user_names.push(link_to voter.user_name, profile_path(voter.user_name), class: 'user-name')
-      end
-      user_names.to_sentence.html_safe + like_plural(votes)
-    end
+    return list_likers(votes) if votes.size <= 2
+    count_likers(votes)
   end
 
   def liked_post(post)
@@ -21,4 +16,20 @@ module PostsHelper
     return ' like this' if votes.count > 1
     ' likes this'
   end
+
+  def list_likers(votes)
+    user_names = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        user_names.push(link_to voter.user_name, profile_path(voter.user_name), class: 'user-name')
+      end
+      user_names.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  def count_likers(votes)
+    vote_count = votes.size
+    vote_count.to_s + ' likes'
+  end
+
 end
